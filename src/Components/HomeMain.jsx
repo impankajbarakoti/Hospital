c4c2021aee788da56552d599019ab2256ec65add
Pankaj Barakoti <pankajbarakoti837@gmail.com>
2025-10-04 17:58:56 +0530

    new

--- src/Components/HomeMain.jsx
+++ src/Components/HomeMain.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import HeroSection from './HeroSection'
-import ServicesSection from './ServicesSection'
-import Home2 from './Home2'
-import AppointmentSection from './AppointmentSection'
-import EmergencySection from './EmergencySection'
-import Testimonials from './Testimonials'
-import WhyChooseUs from './WhyChooseUs'
-import LatestBlogs from './LatestBlogs'
+import React from "react";
+import HeroSection from "./HeroSection";
+import ServicesSection from "./ServicesSection";
+import Home2 from "./Home2";
+import AppointmentSection from "./AppointmentSection";
+import EmergencySection from "./EmergencySection";
+import Testimonials from "./Testimonials";
+import WhyChooseUs from "./WhyChooseUs";
+import LatestBlogs from "./LatestBlogs";
+
 const HomeMain = () => {
   return (
     <div>
-          <HeroSection />
-          <ServicesSection/>
-          <Home2 />
-          <AppointmentSection />
-          <EmergencySection />
-          <Testimonials />
-          <WhyChooseUs />
-          <LatestBlogs/>
+      <HeroSection />
+      <ServicesSection />
+      <Home2 />
+      <AppointmentSection />
+      <EmergencySection />
+      <Testimonials />
+      <WhyChooseUs />
+      <LatestBlogs />
     </div>
-  )
-}
+  );
+};
 
-export default HomeMain
+export default HomeMain;
